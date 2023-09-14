dd9feb10ccf23392a6121655ed968144ef446231
sohan0898 <shuhanur.rahman08@gmail.com>
2023-09-15 01:19:00 +0600

    basic react setup and added tailwind,google font

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center my-6">
+        Course Registration
+      </h1>
+    </>
+  );
+}
+
+export default App;
